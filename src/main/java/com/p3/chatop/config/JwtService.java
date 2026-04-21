69bcd96000c21f630cb8c05d75c80c8094bc7919
Leang Chouteau <chouteau.leang@gmail.com>
2026-04-21 11:53:56 +0200

    maj JwtService.java

--- src/main/java/com/p3/chatop/config/JwtService.java
+++ src/main/java/com/p3/chatop/config/JwtService.java
@@ -1,28 +1,28 @@
 package com.p3.chatop.config;
-
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    private final SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
 
-    private final String SECRET = "secret123456789abcdefg7452102365secret123456789abcdefg7452102365";
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(key)
                 .compact();
     }
 
     public String extractEmail(String token) {
         return Jwts.parser()
-                .setSigningKey(SECRET)
+                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();

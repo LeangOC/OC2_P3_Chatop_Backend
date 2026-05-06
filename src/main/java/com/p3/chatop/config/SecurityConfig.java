0d854a2b8f14e5b8f32589ccaf8bf82903fd71f8
LeangOC <chouteau.leang@gmail.com>
2026-05-06 10:33:23 +0200

    Remove comment from request matchers in SecurityConfig

--- src/main/java/com/p3/chatop/config/SecurityConfig.java
+++ src/main/java/com/p3/chatop/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/uploads/**"   // ✅ IMPORTANT
+                                "/uploads/**"   
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
